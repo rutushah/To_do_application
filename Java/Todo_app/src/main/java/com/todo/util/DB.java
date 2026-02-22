02ba887c29c8d5f29dbebb85e2f4c03974975eb5
rutus-code <rutushah105@gmail.com>
2026-02-21 19:11:03 -0500

    added global db connection

--- Java/Todo_app/src/main/java/com/todo/util/DB.java
+++ Java/Todo_app/src/main/java/com/todo/util/DB.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 
 public class DB {
 
-//    private static String jdbcURL = "jdbc:postgresql://localhost:5432/todo_app";
     private static String jdbcURL = System.getProperty("TODO_DB_URL","jdbc:postgresql://localhost:5432/todo_app");
     private static String username = System.getProperty("TODO_DB_USER", "rutushah");
     private static String password = System.getProperty("TODO_DB_PASS", "todo_pwd");
@@ -15,5 +14,4 @@ public class DB {
         Connection c = DriverManager.getConnection(jdbcURL, username, password);
         return c;
     }
-
 }
